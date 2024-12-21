b8294a7531e5a3026ead39a6336adb14a5fbdc14
Saswat-Kumar-Pradhan <saswatkumar059@gmail.com>
2024-12-21 15:02:40 +0530

    Implemented tab

--- app/models.py
+++ app/models.py
@@ -10,20 +10,24 @@ class Tab(models.Model):
     creation_time = models.DateTimeField(auto_now_add=True)
 
 class Profile(models.Model):
+    tab = models.ForeignKey(Tab, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=100)
     profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
 
 class Event(models.Model):
+    tab = models.ForeignKey(Tab, on_delete=models.CASCADE, default=1)
     date = models.DateField()
     subject = models.CharField(max_length=200)
 
 class Purchase(models.Model):
+    tab = models.ForeignKey(Tab, on_delete=models.CASCADE, default=1)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     products = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Contribution(models.Model):
+    tab = models.ForeignKey(Tab, on_delete=models.CASCADE, default=1)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
